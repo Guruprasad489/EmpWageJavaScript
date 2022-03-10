3ddd2d41238a73c5c438719d3c981bebe0eac83f
Guruprasad489 <guruprasadk.dev@gmail.com>
2022-03-10 13:05:05 +0530

    [Guru] Modified to check employee attendance -UC1

--- EmpWageJavaScript.js
+++ EmpWageJavaScript.js
@@ -1,1 +1,13 @@
 console.log("Welcome to emp wage program using java script")
+
+//UC1 - Check Employee is present or Absent using Math.Random
+const IS_ABSENT=0;
+let empcheck = Math.floor(Math.random()*10)%2;
+if(empcheck==IS_ABSENT)
+{
+    console.log("Employee is absent");
+}
+else
+{
+console.log("Employee is present");
+}
